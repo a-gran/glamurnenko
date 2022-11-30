6c646509dd2921bbe523c2e92cb295ef3e07a58c
Aristarh Gran <awebdeff@gmail.com>
2022-11-30 22:07:15 +0300

    делал фиксацию меню

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+// прикрепление шапки к верху страницы при скролле
+$(function () {
+	let header = $('.header__menu');
 
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1) {
+			header.addClass('header_fixed');
+		} else {
+			header.removeClass('header_fixed');
+		}
+	});
+});
